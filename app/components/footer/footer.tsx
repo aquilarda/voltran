82d3afeb06dc2fb64644336fddf1800230319eec
aquilarda <ahmetardayesildag@gmail.com>
2024-02-25 02:59:58 +0300

    Update Navbar and Footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -2,6 +2,11 @@ import "@/app/components/footer/footer.css"
 
 export default function Footer(){
     return(
-        <main></main>
+        <main>
+            <div className="footer">
+                <div className="footer-left"><h1><a href="/">Voltran</a></h1></div>
+                <div className="footer-right"><h1><a href="https://github.com/aquilarda/"><i>GitHub</i></a></h1></div>
+            </div>
+        </main>
     );
 }
