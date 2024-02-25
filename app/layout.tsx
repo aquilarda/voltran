6b2e35ba89543af0032716920c792d06d899ccbc
aquilarda <ahmetardayesildag@gmail.com>
2024-02-25 03:28:44 +0300

    Update Members Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Home from "@/app/page";
 import Footer from "@/app/components/footer/footer";
 import Navbar from "@/app/components/navbar/navbar";
 import Members from "@/app/members/page";
+import Projects from "@/app/projects/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
