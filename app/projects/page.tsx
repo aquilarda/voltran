59732a8fa5e153157f62363162afdd9ae6a3a757
aquilarda <ahmetardayesildag@gmail.com>
2024-02-25 03:53:16 +0300

    Update Projects Page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,26 @@
-import "@/app/projects/projects.css"
+import "@/app/projects/projects.css";
 
-export default function Projects(){
-    return(
-        <main>Projects</main>
-    );
+export default function Projects() {
+  return (
+    <main>
+        <div className="project-23">
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo magni
+            sed nesciunt perspiciatis repellendus voluptate, doloremque voluptas
+            unde soluta ut provident delectus libero numquam quidem facilis
+            officia aliquid esse doloribus?
+            <img src="/odul.png" alt="Our Prize" className="img-projects my-4"/> <img src="/proje23.png" alt="Our Project in 2023" className="img-projects my-4" />
+          </h1>
+        </div>
+        <div className="project-24">
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cum
+            facilis quisquam aliquid voluptate est reiciendis consequatur,
+            possimus qui nostrum neque ipsam eaque distinctio vel in dolor, ipsa
+            nisi. Praesentium.
+            <img src="/proje24.png" alt="Our Project in 2024" className="img-projects my-4"/>
+          </h1>
+        </div>
+    </main>
+  );
 }
