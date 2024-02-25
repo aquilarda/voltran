e1797ec10fb50b222131ec2e5caaaf0acb37d81b
aquilarda <ahmetardayesildag@gmail.com>
2024-02-25 04:20:07 +0300

    Typo

--- app/page.tsx
+++ app/page.tsx
@@ -7,42 +7,80 @@ export default function Home() {
       <div className="main">
         <div className="left-info-box">
           <span className="left-info-box-title">
-            <h1 className="left-title p-4">Who are we?</h1>
+            <h1 className="left-title p-4">Who we are?</h1>
           </span>
           <span className="left-info-box-text">
             <h3 className="left-box-text p-4">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
-              quam fugiat corrupti cum, fuga modi doloremque suscipit dolorem
-              voluptates unde aliquam voluptas consequatur odio ullam! Eum
-              blanditiis itaque repellendus inventore?
-            </h3>
-          </span>
-          <span><img src="/teamphoto.png" alt="A photograph of our team." className="team-photo"/></span>
-        </div>
-        <div className="right-info-box">
-          <span className="right-info-box-title">
-            <h1 className="right-title p-4">What are we doing?</h1>
-          </span>
-          <span className="right-info-box-text">
-            <h3 className="right-box-text p-4">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
-              quam fugiat corrupti cum, fuga modi doloremque suscipit dolorem
-              voluptates unde aliquam voluptas consequatur odio ullam! Eum
-              blanditiis itaque repellendus inventore?
+              Hello! We are Team Voltran from İzmir/Çiğli Tuğba Özbek Anatolian
+              High School. We are interested in technology, sensitive to nature,
+              and always curious to do better.
             </h3>
           </span>
           <span>
-            <img src="/teamphoto.png" alt="A photograph of our team." className="team-photo"/>
+            <img
+              src="/teamphoto.png"
+              alt="A photograph of our team."
+              className="team-photo"
+            />
+          </span>
+        </div>
+        <div className="right-info-box">
+          <span className="right-info-box-title">
+            <h1 className="right-title p-4">What do we do?</h1>
+          </span>
+          <span className="right-info-box-text">
+            <h3 className="right-box-text p-4">
+              We participate in tournaments such as FLL (First Lego League) and
+              Teknofest, where we strive to do our best as a team while also
+              enjoying quality time together. We share our projects on social
+              media and, with the support of our school, continually strive for
+              improvement.
+            </h3>
+          </span>
+          <span>
+            <img
+              src="/teamphoto.png"
+              alt="A photograph of our team."
+              className="team-photo"
+            />
           </span>
         </div>
       </div>
       <div className="page-box">
-        <h1 className="page-box-h1 text-teal-300">Click <i><u><a href="/members">here</a></u></i> to see our members.</h1>
-        <h1 className="page-box-h1 text-teal-300">Click <i><u><a href="/projects">here</a></u></i> to see our projects.</h1>
-        <h1 className="page-box-a text-teal-300"><a href="https://www.instagram.com/fll_voltran/"><u><i>Instagram</i></u></a></h1>
-        <h1 className="page-box-a text-teal-300"><a href="https://www.youtube.com/@TugbaOzbek-zn9br"><u><i>Youtube</i></u></a></h1>
+        <h1 className="page-box-h1 text-teal-300">
+          Click{" "}
+          <i>
+            <u>
+              <a href="/members">here</a>
+            </u>
+          </i>{" "}
+          to see our members.
+        </h1>
+        <h1 className="page-box-h1 text-teal-300">
+          Click{" "}
+          <i>
+            <u>
+              <a href="/projects">here</a>
+            </u>
+          </i>{" "}
+          to see our projects.
+        </h1>
+        <h1 className="page-box-a text-teal-300">
+          <a href="https://www.instagram.com/fll_voltran/">
+            <u>
+              <i>Instagram</i>
+            </u>
+          </a>
+        </h1>
+        <h1 className="page-box-a text-teal-300">
+          <a href="https://www.youtube.com/@TugbaOzbek-zn9br">
+            <u>
+              <i>Youtube</i>
+            </u>
+          </a>
+        </h1>
       </div>
-      <img src="/favicon.png" alt="Voltran Logo" className="img-logo"/>
+      <a href="/"><img src="/favicon.png" alt="Voltran Logo" className="img-logo" /></a>
     </main>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,24 +3,30 @@ import "@/app/projects/projects.css";
 export default function Projects() {
   return (
     <main>
-        <div className="project-23">
-          <h1>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo magni
-            sed nesciunt perspiciatis repellendus voluptate, doloremque voluptas
-            unde soluta ut provident delectus libero numquam quidem facilis
-            officia aliquid esse doloribus?
-            <img src="/odul.png" alt="Our Prize" className="img-projects my-4"/> <img src="/proje23.png" alt="Our Project in 2023" className="img-projects my-4" />
-          </h1>
-        </div>
-        <div className="project-24">
-          <h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cum
-            facilis quisquam aliquid voluptate est reiciendis consequatur,
-            possimus qui nostrum neque ipsam eaque distinctio vel in dolor, ipsa
-            nisi. Praesentium.
-            <img src="/proje24.png" alt="Our Project in 2024" className="img-projects my-4"/>
-          </h1>
-        </div>
+      <div className="project-23">
+        <h1>
+          In the 2022-2023 First Lego League season, we won the Innovative
+          Project 2nd place award with our 'Street Lamp Improvement Project'.
+          <img src="/odul.png" alt="Our Prize" className="img-projects my-4" />
+          <img
+            src="/proje23.png"
+            alt="Our Project in 2023"
+            className="img-projects my-4"
+          />
+        </h1>
+      </div>
+      <div className="project-24">
+        <h1>
+          We are counting down the days with excitement to find out our ranking
+          for the 2023-2024 season with our project developed using the art of
+          'installation'."
+          <img
+            src="/proje24.png"
+            alt="Our Project in 2024"
+            className="img-projects my-4"
+          />
+        </h1>
+      </div>
     </main>
   );
 }
